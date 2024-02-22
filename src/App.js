01a2e526b1b998045d2c0691e1464ac20bc1b970
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-22 22:11:43 +0200

    Add spinner effect until the data is fetched

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import Header from "./components/header/Header";
 import Trailer from "./components/trailer/Trailer";
 import Reviews from "./components/reviews/Reviews";
 import NotFound from "./components/notFound/NotFound";
+import { SpinnerLoading } from "./components/SpinnerLoading";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
 
       setMovies(response.data);
+      setIsLoading(false);
     } catch (err) {
+      setIsLoading(false);
       console.log(err);
     }
   };
@@ -45,24 +49,28 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home movies={movies} />}></Route>
-          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
-          <Route
-            path="/Reviews/:movieId"
-            element={
-              <Reviews
-                getMovieData={getMovieData}
-                movie={movie}
-                reviews={reviews}
-                setReviews={setReviews}
-              />
-            }
-          ></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Route>
-      </Routes>
+      {isLoading ? (
+        <SpinnerLoading />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home movies={movies} />}></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+            <Route
+              path="/Reviews/:movieId"
+              element={
+                <Reviews
+                  getMovieData={getMovieData}
+                  movie={movie}
+                  reviews={reviews}
+                  setReviews={setReviews}
+                />
+              }
+            ></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Route>
+        </Routes>
+      )}
     </div>
   );
 }
