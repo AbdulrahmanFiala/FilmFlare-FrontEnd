72b81a071cefc20ec18c67015ab32237cc7d9c8c
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2024-02-22 16:43:57 +0200

    Replacing the default logo with FilmFlare Logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import filmFlareLogo from "../../images/FilmFlareLogo.png";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -11,8 +12,8 @@ const Header = () => {
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container fluid>
         <Navbar.Brand href="/" style={{ color: "gold" }}>
-          <FontAwesomeIcon icon={faVideoSlash} />
-          Gold
+          <img src={filmFlareLogo} id="logo" />
+          FilmFlare
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
